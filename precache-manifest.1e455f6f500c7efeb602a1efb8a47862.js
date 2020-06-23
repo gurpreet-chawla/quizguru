f876f36612710a7522a458a715cf9ef5c5bf56f2
Gurpreet Singh <singhg469@gmail.com>
2020-06-23 08:03:46 +0530

    Updates

---
+++ precache-manifest.1e455f6f500c7efeb602a1efb8a47862.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87cd79efcccc00d302d71fe57434106d",
+    "url": "/quizguru/index.html"
+  },
+  {
+    "revision": "939cdc643c69174a37e5",
+    "url": "/quizguru/static/css/2.d408af97.chunk.css"
+  },
+  {
+    "revision": "0b546832d1860537d8bc",
+    "url": "/quizguru/static/css/main.784ce376.chunk.css"
+  },
+  {
+    "revision": "939cdc643c69174a37e5",
+    "url": "/quizguru/static/js/2.4847d415.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/quizguru/static/js/2.4847d415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b546832d1860537d8bc",
+    "url": "/quizguru/static/js/main.d40d5e8e.chunk.js"
+  },
+  {
+    "revision": "181d0ea6d01eab6d1927",
+    "url": "/quizguru/static/js/runtime-main.8888019d.js"
+  },
+  {
+    "revision": "c9afcc9efad49c9a2be5f75e33eadf67",
+    "url": "/quizguru/static/media/Wrong-answer.c9afcc9e.mp3"
+  },
+  {
+    "revision": "3fa111b05fcc4ffe481c3297a2e5125d",
+    "url": "/quizguru/static/media/instruction.3fa111b0.png"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/quizguru/static/media/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/quizguru/static/media/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/quizguru/static/media/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/quizguru/static/media/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "eebffc6a1214c484a8f56841554c37af",
+    "url": "/quizguru/static/media/myphoto.eebffc6a.png"
+  },
+  {
+    "revision": "90a6d5af1ca2c7dc0bd195127594dc56",
+    "url": "/quizguru/static/media/next.90a6d5af.mpeg"
+  },
+  {
+    "revision": "b9f9fcc1969fb6cde417124ae87c20f3",
+    "url": "/quizguru/static/media/quize.b9f9fcc1.png"
+  },
+  {
+    "revision": "86576bb5fc5002b0650ee53b4ca39bc5",
+    "url": "/quizguru/static/media/right-answer.86576bb5.mpeg"
+  }
+]);
